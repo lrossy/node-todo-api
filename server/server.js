ba2949dc07cd242f1f433358fcfa153661540cf0
Luke <lrossy@gmail.com>
2017-07-19 10:28:41 -0400

    just saving things

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || '3000';
+
 
 app.use(bodyParser.json());
 
@@ -47,7 +49,6 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
-var port = process.env.port ? process.env.port : '3000';
 
 app.listen(port, () =>{
     console.log(`Started on port ${port}`)
